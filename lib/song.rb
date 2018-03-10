1f052ac1ac09fc9ad55469b059324e535360c641
Kelsey Fecho <kfecho30@gmail.com>
2018-03-10 01:48:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,9 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist
+    if !@@artists.include? artist
+      @@artists << artist
+    end
     if !@@genres.include? genre
       @@genres << genre
     end
